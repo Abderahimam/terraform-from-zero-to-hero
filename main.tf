3b4ba9e5d544c8ecb5a238b36eb356ef3008cd16
Amari <abderahim.amari@capgemini.com>
2026-03-03 09:21:08 +0100

    created resource group

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.62.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+}
+   
+resource "azurerm_resource_group" "financegrp" {
+  name  = "financegrp"
+  location = "polandcentral"
+}
